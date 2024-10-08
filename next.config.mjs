89b29817ccc481addc21ff7855e1e1a91e5719a2
Aparna Joshi <aparnajoshi.88@gmail.com>
2024-10-08 10:35:16 +0200

    allow @ paths from nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, './'),
+        };
+
+        return config;
+    },
 };
 
 export default nextConfig;
