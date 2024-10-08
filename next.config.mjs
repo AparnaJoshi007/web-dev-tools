3d2cbd62359018b4c2aed64b12897c4c928717d7
Aparna Joshi <aparnajoshi.88@gmail.com>
2024-10-08 22:59:54 +0200

    Add trailing slash for static content serve

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const __dirname = new URL('.', import.meta.url).pathname;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: true,
     output: 'export',
     webpack: (config) => {
         config.resolve.alias = {
